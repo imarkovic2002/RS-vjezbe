f0850ca7aea1b97fd396895f3736873755a48bf8
imarkovic2002 <markovici089@gmail.com>
2024-11-20 11:32:04 +0100

    RS03-zadaci sa sata(1. i 2. zadatak)

---
+++ 4_vjezbe/app.py
@@ -0,0 +1,30 @@
+## RS3 - 20.11.2024.
+## Kod sa sata 
+## Marković Igor
+
+import asyncio
+async def korutina():
+    lista = [i for i in range(1,11)]
+    await asyncio.sleep(3)
+    print ('Podaci su dohvaćeni.')
+    return lista
+
+asyncio.run(korutina())
+
+# 2. Zadatak
+async def korutina_1():
+    print('Dohvaćam podatke o korisniku...')
+    await asyncio.sleep(3)
+    print('Podaci o korisniku dohvaćeni...')
+    return {'api_1': 'uspješno'}
+
+async def korutina_2():
+    print('Dohvaćam podatke o proizvodu...')
+    await asyncio.sleep(5)
+    print('Podaci o proizvodu dohvaćeni.')
+    return {'api_2': 'uspješno'}
+
+async def main():
+    podaci_1, podaci_2 = await asyncio.gather(korutina_1(), korutina_2())
+
+asyncio.run(main())
