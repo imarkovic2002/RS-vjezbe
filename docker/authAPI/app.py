34402717ced0c68bdfdaddc7eed50c49ec73835f
imarkovic2002 <markovici089@gmail.com>
2025-01-29 09:21:48 +0100

    kod iz skripte-docker

---
+++ docker/authAPI/app.py
@@ -0,0 +1,39 @@
+import hashlib
+from aiohttp import web
+
+app = web.Application()
+
+korisnici = [
+    {"korisnicko_ime": "admin", "lozinka_hash": "8d43d8eb44484414d61a18659b443fbfe52399510da4689d5352bd9631c6c51b"},  # lozinka = "lozinka123"
+    {"korisnicko_ime": "markoMaric", "lozinka_hash": "5493c883d2b943587ea09ab8244de7a0a88d331a1da9db8498d301ca315d74fa"},  # lozinka = "markoKralj123"
+    {"korisnicko_ime": "ivanHorvat", "lozinka_hash": "a31d1897eb84d8a6952f2c758cdc72e240e6d6d752b33f23d15fd9a53ae7c302"},  # lozinka = "lllllllllllozinka_123"
+    {"korisnicko_ime": "Nada000", "lozinka_hash": "492f3f38d6b5d3ca859514e250e25ba65935bcdd9f4f40c124b773fe536fee7d"}  # lozinka = "blablabla"
+]
+
+def hash_data(data: str) -> str:
+    return hashlib.sha256(data.encode()).hexdigest()
+
+async def add_korisnik(request):
+    try:
+        data = await request.json()
+    except Exception:
+        return web.json_response({'error': 'Neispravan JSON format'}, status=400)
+    if 'korisnicko_ime' not in data or 'lozinka' not in data:
+        return web.json_response({'error': 'Nedostaju korisnicko_ime ili lozinka'}, status=400)
+    korisnicko_ime = data['korisnicko_ime']
+    lozinka = data['lozinka']
+    if korisnicko_ime in [korisnik["korisnicko_ime"] for korisnik in korisnici]:
+        return web.json_response({'error': 'Korisnik već postoji'}, status=400)
+    lozinka_hash = hash_data(lozinka)
+    novi_korisnik = {
+        "korisnicko_ime": korisnicko_ime,
+        "lozinka_hash": lozinka_hash,
+    }
+    korisnici.append(novi_korisnik)
+    return web.json_response(novi_korisnik, status=201)
+
+
+app.router.add_post('/register', add_korisnik)
+
+if __name__ == '__main__':
+    web.run_app(app, host='0.0.0.0', port=9000)
---
+++ docker/compose-example/aiohttp-regije/app.py
@@ -0,0 +1,32 @@
+from aiohttp import web
+import asyncio
+
+app = web.Application()
+
+dummy_podaci_regije = [
+    {"kljuc": "sredisnja", "naziv": "Središnja Hrvatska", "gradovi": ["Zagreb", "Karlovac", "Sisak"]},
+    {"kljuc": "istocna", "naziv": "Istočna Hrvatska", "gradovi": ["Osijek", "Slavonski Brod", "Vinkovci", "Vukovar"]},
+    {"kljuc": "gorska", "naziv": "Gorska Hrvatska", "gradovi": ["Delnice", "Čabar", "Vrbovsko"]},
+    {"kljuc": "unutrasnjost Dalmacije", "naziv": "Unutrašnjost Dalmacije", "gradovi": ["Knin", "Sinj", "Imotski"]},
+    {"kljuc": "sjeverni Jadran", "naziv": "Sjeverni Jadran", "gradovi": ["Rijeka", "Pula", "Opatija", "Rovinj"]},
+    {"kljuc": "srednji Jadran", "naziv": "Srednji Jadran", "gradovi": ["Split", "Zadar", "Šibenik"]},
+    {"kljuc": "juzni Jadran", "naziv": "Južni Jadran", "gradovi": ["Dubrovnik", "Metković", "Ploče"]}
+]
+
+async def get_regije(request):
+    return web.json_response(dummy_podaci_regije)
+
+async def get_regija(request):
+    kljuc = request.match_info['kljuc']
+    for regija in dummy_podaci_regije:
+        if regija["kljuc"] == kljuc:
+            return web.json_response(regija)
+        return web.json_response({'error': 'Regija nije pronađena.'}, status = 404)
+
+
+
+
+app.router.add_get("/regije", get_regije)
+app.router.add_post("regije/{kljuc}", get_regija)
+
+web.run_app(app, port=4000)
